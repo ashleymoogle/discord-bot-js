c80026dbe8140e1217ec1806018e75ba309a2b99
Valentin Roudge <v.roudge@gmail.com>
2017-06-07 17:02:00 +0300

    Update logsLogic.js

--- controllers/logsLogic.js
+++ controllers/logsLogic.js
@@ -2,18 +2,19 @@ import fs from 'fs-extra'
 import config from '../config'
 
 export default function logsLogic(client) {
-    let guild = client.guilds.get(config.guildId);
-    let activityChannel = guild.channels.get(config.channelActivityId);
-    let textChannels = guild.channels.reduce((acc, chan) => {
-        if(chan.type === 'text') {
-            acc.push(chan)
-        }
+    const guild = client.guilds.get(config.guildId);
+    const activityChannel = guild.channels.get(config.channelActivityId);
+    
+    const textChannels = guild.channels.reduce((acc, chan) => {
+        chan.type === 'text') ? acc.push(chan) : null;
         return acc
     }, []);
+    
     const dir = config.logsPath;
     fs.ensureDirSync(`${dir}/${guild.name}`);
+    
     textChannels.forEach((chan) => {
         fs.ensureFileSync(`${dir}/${guild.name}/${chan.name}.md`);
     });
-    return {guild: guild, activityChannel: activityChannel, textChannels: textChannels}
+    return {guild,  activityChannel, textChannels}
 }
